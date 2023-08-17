49bfd1dce384b4097c3c2d9bd638fec79c0f553b
Anandh Kumar <accetanandhmca@gmail.com>
2023-08-17 17:34:05 +0530

    FUNCTIONALITY UPDATE

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoItem = ({ todo, onEdit, onDelete, onStatusChange }) => {
+
   const getSelectClassName = () => {
     if (todo.status === "completed") {
       return "btn btn-success"; // Apply green color for completed todos
@@ -10,26 +11,88 @@ const TodoItem = ({ todo, onEdit, onDelete, onStatusChange }) => {
     return "btn btn-info"; // Default color (info)
   };
 
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTodo, setEditedTodo] = useState({ ...todo });
+
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
+
+  const handleSaveEdit = () => {
+    onEdit(todo.id, editedTodo); // Call onEdit with the updated todo
+    setIsEditing(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setEditedTodo((prevTodo) => ({
+      ...prevTodo,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="todo-card">
-      <p>Name : {todo.name}</p>
-      <p>Description : {todo.description}</p>
+      <p>
+        Name :{" "}
+        {isEditing ? (
+          <input
+            name="name"
+            value={editedTodo.name}
+            onChange={handleInputChange}
+          />
+        ) : (
+          todo.name
+        )}
+      </p>
+      <p>
+        Description :{" "}
+        {isEditing ? (
+          <input
+            name="description"
+            value={editedTodo.description}
+            onChange={handleInputChange}
+          />
+        ) : (
+          todo.description
+        )}
+      </p>
       <label>Status : </label>
       <select
         className={getSelectClassName()}
-        value={todo.status}
+        value={isEditing ? editedTodo.status : todo.status}
         onChange={(e) => onStatusChange(todo.id, e.target.value)}
       >
-        <option value="notCompleted" className="bg-danger text-white">Not Completed</option>
-        <option value="completed" className="complete bg-success text-white">Completed</option>
+        <option value="notCompleted" className="bg-danger text-white">
+          Not Completed
+        </option>
+        <option value="completed" className="complete bg-success text-white">
+          Completed
+        </option>
       </select>
-      <br></br>
-      <button className="btn btn-info" onClick={() => onEdit(todo.id)}>
-        Edit
-      </button>
-      <button className="btn btn-danger" onClick={() => onDelete(todo.id)}>
-        Delete
-      </button>
+      <br />
+      {isEditing ? (
+        <>
+          <button className="btn btn-success" onClick={handleSaveEdit}>
+            Save
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => setIsEditing(false)}
+          >
+            Cancel
+          </button>
+        </>
+      ) : (
+        <>
+          <button className="btn btn-info" onClick={handleEdit}>
+            Edit
+          </button>
+          <button className="btn btn-danger" onClick={() => onDelete(todo.id)}>
+            Delete
+          </button>
+        </>
+      )}
     </div>
   );
 };
